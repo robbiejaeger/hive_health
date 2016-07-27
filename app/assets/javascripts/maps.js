d8c00a00624c3afaf6c0252ccbeac24bc86a05db
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-27 14:29:12 -0600

    Add google js map with all sites and markers

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,11 +1,55 @@
 $(document).ready(function(){
-  initMap();
+
+  $.ajax({
+      url: "http://localhost:3000/api/v1/sites",
+      method: "GET",
+      dataType: "JSON",
+      success: function(data){
+                initMap(data);
+              }
+  });
+
 });
 
-function initMap(){
+function initMap(data){
   var mapDiv = document.getElementById('all-sites-map');
+
   var map = new google.maps.Map(mapDiv, {
       center: {lat: 39.83, lng: -98.58},
       zoom: 4
   });
+
+  var setMarkers = function(data){
+      for (var i = 0; i < data.length; i++){
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        var contentString = '<div id="content">'+
+                  '<div id="siteNotice">'+
+                  '</div>'+
+                  '<h1 id="firstHeading" class="firstHeading">' + data[i].name + '</h1>'+
+                  '<div id="bodyContent">'+
+                  '<p>Marker content!</p>'+
+                  '</div>'+
+                  '</div>';
+
+        var siteLatLng = {lng: parseFloat(data[i].latitude), lat: parseFloat(data[i].longitude)};
+
+        var marker = new google.maps.Marker({
+                position: siteLatLng,
+                map: map,
+                title: 'Hello World!',
+                info: contentString
+        });
+
+        google.maps.event.addListener(marker, 'click', function(){
+          infowindow.setContent(this.info);
+          infowindow.open(map, this);
+        });
+      }
+  };
+
+  setMarkers(data);
 };
