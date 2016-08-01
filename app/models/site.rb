60e1af51e13d5aba339feaf3f3d3ca41211dd02c
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-31 21:56:59 -0600

    Can get site's followers

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,7 +2,9 @@ class Site < ApplicationRecord
   belongs_to :user
   has_many :hives
   has_many :logs, through: :hives
-  has_many :follows
+
+  has_many :follower_relationships, foreign_key: :site_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :user
 
   validates :street, uniqueness: { scope: [:city, :state, :zip] }, on: :create
   validates :street, presence: true
