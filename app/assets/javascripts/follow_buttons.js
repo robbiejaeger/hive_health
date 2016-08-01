8e687cae03a49892bd045e1d9dc8e4f28c159bf7
Robbie Jaeger <jaeger.rob@gmail.com>
2016-08-01 15:42:42 -0600

    Change jquery follow button to recognize changed dom

--- app/assets/javascripts/follow_buttons.js
+++ app/assets/javascripts/follow_buttons.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
-  $("#follow-button").on("ajax:success", function(){
+  // $.ajax({
+  //
+  // }).done(function() {
+  //   $( this ).addClass( "done" );
+  // });
+
+  $("body").on("ajax:success", "#follow-button", function(){
     $(this).text('Unfollow This Site\'s Hives');
-    $(this).attr('class', 'btn btn-primary')
-    $(this).attr('href', '/' + $(this).data("id") + '/unfollow_site')
-    $(this).attr('id', 'unfollow-button')
+    $(this).attr('href', '/' + $(this).data("id") + '/unfollow_site');
+    $(this).attr('id', 'unfollow-button');
   });
 
-  $("#unfollow-button").on("ajax:success", function(){
+  $("body").on("ajax:success", "#unfollow-button", function(){
     $(this).text('Follow This Site\'s Hives');
-    $(this).attr('class', 'btn btn-primary')
-    $(this).attr('href', '/' + $(this).data("id") + '/follow_site')
-    $(this).attr('id', 'follow-button')
+    $(this).attr('href', '/' + $(this).data("id") + '/follow_site');
+    $(this).attr('id', 'follow-button');
   });
 });
