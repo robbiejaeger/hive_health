df28593ddeef1fcde07016b8d58af3fb7fccfd35
Robbie Jaeger <jaeger.rob@gmail.com>
2016-08-03 14:25:50 -0600

    Move weather forecast to each site page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController < ApplicationController
 
   def show
     @sites = current_user.sites
-    @weather_days = Weather.forecast_by_location(@sites[0].latitude, @sites[0].longitude)
     @sites_following_logs = current_user.logs_of_followed_sites
   end
 end
