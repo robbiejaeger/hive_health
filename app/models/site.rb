d98f683d02d8d571ce966957d1215c7cb18b003a
Robbie Jaeger <jaeger.rob@gmail.com>
2016-08-08 10:06:26 -0600

    Can add site images on development

--- app/models/site.rb
+++ app/models/site.rb
@@ -6,7 +6,7 @@ class Site < ApplicationRecord
   has_many :follower_relationships, foreign_key: :site_id, class_name: 'Follow'
   has_many :followers, through: :follower_relationships, source: :user
 
-  has_attached_file :site_img, style: {
+  has_attached_file :site_img, styles: {
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
