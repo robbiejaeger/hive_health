3c15e898bd8a9a0991e5b820becabd741f191fa7
Robbie Jaeger <jaeger.rob@gmail.com>
2016-08-09 10:59:40 -0600

    API key created when user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: :user_id, class_name: 'Follow'
   has_many :following, through: :following_relationships, source: :site
 
+  before_create :generate_api_key
+
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid = auth_info.uid
@@ -14,6 +16,11 @@ class User < ApplicationRecord
       new_user.token = auth_info.credentials.token
       new_user.expires_at = auth_info.credentials.expires_at
     end
+  end
+
+  def generate_api_key
+    token = Digest::SHA2.new << "#{uid}#{email}"
+    self.api_key = token.to_s
   end
 
   def follow(site)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809150927) do
+ActiveRecord::Schema.define(version: 20160809162147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20160809150927) do
     t.string   "expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "api_key"
   end
 
   add_foreign_key "follows", "sites"
